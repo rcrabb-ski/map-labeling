9b14c8d024cdc0a14374a90cf0defbac07ec8696
Ryan Crabb <79174367+rcrabb-ski@users.noreply.github.com>
2022-04-27 10:57:32 -0700

    Input dialog for class/orientation
    No longer requires the manual hard coding of orientation and class.  This approach allows for multiple orientations to be entered, so only one click per landmark.

--- sign_labeler.m
+++ sign_labeler.m
@@ -1,49 +1,61 @@
-function strout = sign_labeler(XY)
-map = imread('/Users/rcrabb/PycharmProjects/ParticleFilter/maps/walls_1.bmp');
-mapfig = figure;
-imshow(map);
-if isempty(XY)
-    [xi, yi] = getpts(mapfig);
-    xi = floor(xi);
-    yi = floor(yi);
-    XY = [xi, yi]';
-else
-    xi = XY(1,:)';
-    yi = XY(2,:)';
+function strout = sign_labeler(mapfile)
+% SIGN_LABELER( mapfile, XY)
+% input:
+%   mapfile - file location for bitmap of map to be labeled
+%   XY (optional) - preset cooordinates
+
+% List of class names and dialog text for classes
+classList = {'Caution', 'Exit', 'FaceMask', 'James', 'Manfred', ...
+           'NamePlate',  'OneWay', 'RERCLarge', 'RedFire', 'Restroom', ...
+           'Safety', 'SixFeet'};
+classdlg = 'Enter class:';
+for c = 1:length(classList)
+    classdlg = [classdlg newline num2str(c) ' - ' classList{c}];
 end
+
+% List of directions for dialog box
+orientdlg = 'Enter Orientation (neswud) - multiple characters allowed';
 % Coordinate Translation
-w = [ -1 0]';
-e = [1 0]';
-s = [ 0 -1]';
-n = [0 1]';
-ns = [0 1; 0 -1]';
-ew = [1 0; -1 0]';
-% Sign Data
+w = [-1  0  0]';
+e = [ 1  0  0]';
+s = [ 0 -1  0]';
+n = [ 0  1  0]';
+u = [ 0  0  1]';
+d = [ 0  0 -1]';
+
+% Load and display the map
+map = imread(mapfile);
+mapfig = figure;
+title('Choose landmark location.  Close window if complete')
+imshow(map);
+
+% Create data structures
+X = [];
+Y = [];
 classes = [];
 orientation = [];
-xi = [];
-yi = [];
-close(mapfig)
-for xy = XY
-    mapCopy = map;
-    imshow(mapCopy);
-    hold on
-    plot(xy(1), xy(2), 'co', 'MarkerSize', 7);
-    plot(xy(1), xy(2), 'c+', 'MarkerSize', 7);
-    class = input('Class number: ');
-    ori = input('Orienation (n, e, s, w, ew, ns): ');
-    classes = [classes class*ones(1,numel(ori)/2)];
-    xi = [xi xy(1)*ones(1,numel(ori)/2)];
-    yi = [yi xy(2)*ones(1,numel(ori)/2)];
-    orientation = [orientation ori];
-    close all
+
+% Get coordinates one click at a time
+point = drawpoint;
+while (isprop(point,'Position'))
+    pt = point.Position;
+    usrInput = inputdlg({classdlg, orientdlg}, 'Class and Orientation');
+    for ori = usrInput{2}
+        X(end+1) = floor(pt(1));
+        Y(end+1) = floor(pt(2));
+        classes(end+1) = str2num(usrInput{1});
+        orientation = [orientation eval(ori)];
+    end
+    point = drawpoint;
 end
-save(replace(string(datetime),":","-")+".mat","classes","orientation","yi", "xi");
+
+save(replace(string(datetime),":","-")+".mat","classes","orientation","Y", "X");
 strout = sprintf('[');
-for i = 1:length(xi)
-    strout = append(strout,sprintf('{"Col": %d, "Row": %d, "n_u": %d, "n_v": %d, "class": %d},\n',xi(i), yi(i), orientation(1,i), orientation(2,i), classes(i)));
+for i = 1:length(X)
+    strout = append(strout,sprintf('{"Col": %d, "Row": %d, "n_u": %d, "n_v": %d, "n_w": %d, "class": "%s"},\n',...
+        X(i), Y(i), orientation(1,i), orientation(2,i), orientation(2,i), classList{classes(i)}));
 end
 strout(end-1:end+1) = ']\n';
 fprintf(strout);
-save(replace(string(datetime),":","-")+".mat","classes","orientation","yi", "xi","strout");
+save(replace(string(datetime),":","-")+".mat","classes","orientation","Y", "X","strout");
 
